3a6af0495b743e43b3a2621416443e5f2510a1aa
Unknown <theinsomniacgamemaker@email.com>
2018-12-02 21:01:11 -0500

    Scrolling with Respawning Implemented
    There are some gaps when respawning.

---
+++ Résumé/Assets/Scripts/RespawnOnInvisible.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnOnInvisible : MonoBehaviour
+{
+    [SerializeField]
+    private Transform spawnPosition;
+
+    private void OnBecameInvisible()
+    {
+        //Debug.Break();
+        transform.position = new Vector2(spawnPosition.position.x, transform.position.y);
+    }
+
+
+
+}
