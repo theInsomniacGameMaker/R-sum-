eefaee91d7fe749b2e53e9be26dc322f09637004
Unknown <theinsomniacgamemaker@email.com>
2018-12-02 20:30:17 -0500

    Background Scrolling
    The background is scrolling but not re-spawning.

---
+++ Résumé/Assets/Scripts/Scroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private Vector2 direction;
+
+    void Start ()
+    {
+        direction.Normalize();
+	}
+	
+	void Update ()
+    {
+        if (speed == 0.0f)
+            Debug.Log("Speed is set to 0 for " + gameObject.name);
+
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
