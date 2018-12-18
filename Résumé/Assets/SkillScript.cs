bd6e99181b688cb33d9df6eb2ee126d57950094c
Unknown <theinsomniacgamemaker@email.com>
2018-12-17 22:36:48 -0500

    Add New Things
    Added Canvas and other things to start working on the animation for  the panel and text .

---
+++ Résumé/Assets/SkillScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillScript : MonoBehaviour
+{
+    private bool hasBeenCollected;
+    Vector2 startPostion;
+    RectTransform selfRectTransform;
+    private void Start()
+    {
+        selfRectTransform = GetComponent<RectTransform>();
+
+        if (!selfRectTransform)
+        {
+            Debug.Log("This object does not have a RectTranform");
+        }
+
+        startPostion = selfRectTransform.localPosition;
+    }
+
+    private IEnumerator MoveToPosition(Vector2 moveBy)
+    {
+        Vector2 desiredPosition = startPostion + moveBy;
+        while ((Vector2)selfRectTransform.localPosition != desiredPosition)
+        {
+            selfRectTransform.localPosition = Vector2.Lerp(selfRectTransform.localPosition, desiredPosition, Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    
+
+
+}
