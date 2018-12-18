a0a4a78b9756890e210515c0b2580f3fa8da5c4b
Unknown <theinsomniacgamemaker@email.com>
2018-12-18 13:14:20 -0500

    Added Skills

---
+++ Résumé/Assets/SortChildren.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortChildren : MonoBehaviour
+{
+    private void OnValidate()
+    {
+        int childCount = transform.childCount;
+        for (int i = 0; i < childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<RectTransform>().localPosition = new Vector2(0, (i - (childCount / 2)) * 100); 
+        }
+    }
+}
