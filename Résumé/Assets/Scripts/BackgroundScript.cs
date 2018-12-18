ae9c5bdaf17af782969a8454adb571cbf82af9ce
Unknown <theinsomniacgamemaker@email.com>
2018-12-17 20:40:31 -0500

    Changed Name of Script

--- Résumé/Assets/Scripts/BackgroundScript.cs
+++ Résumé/Assets/Scripts/BackgroundScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RespawnOnInvisible : MonoBehaviour
+public class BackgroundScript : MonoBehaviour
 {
     [SerializeField]
     private Transform spawnPosition;
